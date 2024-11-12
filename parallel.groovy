712c06c7f7615b1226c9f8d7edea52df654948b1
Andrew A. Bille <a.bille@postgrespro.ru>
2024-11-12 21:27:25 +0700

    Test

--- parallel.groovy
+++ parallel.groovy
@@ -1,17 +1,17 @@
-node('local') {
-    String[] arr = [ "one","two","three",'four','five' ]
-    echo "Running commands: ${arr}"
-    int top = arr.size()
-    echo "top is ${top}"
-    def stepsForParallel = [:]
+String[] arr = [ "one","two","three",'four','five' ]
+echo "Running commands: ${arr}"
+int top = arr.size()
+echo "top is ${top}"
+def stepsForParallel = [:]
 
-    for (int i = 0; i < arr.size(); i++) {
-        def it = arr[i]
-        def stepName = "running ${it}"
-        stepsForParallel[stepName] = { ->
-            echo "${it}"
-            sh "python3 -c 'import random; exit((int)(random.random()>0.5))'"
-        }
+for (int i = 0; i < arr.size(); i++) {
+    def it = arr[i]
+    def stepName = "running ${it}"
+    stepsForParallel[stepName] = { ->
+        echo "${it}"
+        sh "python3 -c 'import random; exit((int)(random.random()>0.5))'"
     }
+}
+node('local') {
     parallel stepsForParallel
 }
